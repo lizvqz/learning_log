484ddbdc3152fc4bb7960dd2c66dc2f5aff82185
Liz Vazquez <vqz.gloria@gmail.com>
2019-11-08 14:59:01 -0500

    second comit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('topics/', views.topics, name = 'topics'),
     # Detail page for a single topic.
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name = 'new_topic'),
+    # Page for adding a new entry
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name = 'new_entry'),
 ]
